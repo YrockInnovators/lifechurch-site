ee07e47793a33ae48b5b729aadbebafeb30965f3
Adam Fairhead <adam@adamfairhead.com>
2014-06-10 11:46:13 -0500

    Add navigation item for Live

--- js/components/church_online_counter.js
+++ js/components/church_online_counter.js
@@ -2,7 +2,7 @@ jQuery(function() {
 
   // Live color changing
   setInterval(function() {
-    $('.nav-watch .live').toggleClass('alt');
+    $('.nav-live.is-live').toggleClass('alt');
   }, 1000);
 
   // The rest
@@ -10,7 +10,7 @@ jQuery(function() {
 
   var days, goLive, hours, intervalId, minutes, seconds;
   goLive = function() {
-    $('.nav-watch .live').removeClass('hide');
+    $('.nav-live').removeClass('is-live');
     $('[data-churchonline-counter]').text("Live Now").addClass("live");
   };
   days = void 0;
