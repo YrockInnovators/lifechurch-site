21f25875bb45c3c418a8d521b900903e03b25da6
Adam Fairhead <adam@adamfairhead.com>
2014-08-18 10:52:25 -0500

    Phone numbers to involved items

--- js/production.js
+++ js/production.js
@@ -153,6 +153,39 @@ $(document).ready(function(){
 });
 
 $(function() {
+  // Keep Involved form users on the site (with number)
+  $('.form-involved.with-number').submit(function(event) {
+    event.preventDefault();
+
+    var form = $(this);
+
+    var recipient = form.find('input[name="recipient"]').val();
+    var subject = form.find('input[name="subject"]').val();
+    var name = form.find('input[name="name"]').val();
+    var phone = form.find('input[name="phone"]').val();
+    var number = form.find('input[name="number"]').val();
+    var detail = name + ": " + phone + ", " + number;
+
+    if (name.replace(/ /g,'').length == 0 || phone.replace(/ /g,'').length == 0)
+    {
+      form.addClass('validation');
+      return false;
+    }
+    else {
+      $.post(form.attr('action'), {
+        recipient: recipient,
+        subject: subject,
+        detail: detail,
+        key: '345e8e6fb8'
+      });
+
+      form.parent().fadeOut();
+      form.parent().parent().append("<div class='cta-thanks' style='display: none;'><hr /><em>Thanks! We'll be in touch soon.</em></div>").fadeIn();
+      form.parent().parent('.cta').removeClass('open').addClass('sent');
+      form.parent().parent().find('.cta-thanks').fadeIn();
+    }    
+  });
+
   // Keep Involved form users on the site
   $('.form-involved').submit(function(event) {
     event.preventDefault();
