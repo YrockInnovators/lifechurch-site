f56a804d8646ab3f2cf48facf161357b5bf13d74
Adam Fairhead <adam@adamfairhead.com>
2014-09-03 15:20:11 -0500

    Connect Geolify test

--- js/pages/connect.js
+++ js/pages/connect.js
@@ -1,21 +1,32 @@
 $(document).ready(function () {
 
   if ($('.location-identifier').length > 0) {
-    if($('.location-identifier').contains("no-location")) {
-      $('.form-no-location').show();
+    $(".location-chooser").change(function(){
+      if ($('.location-chooser select').val() == "wichita") {
+        $('.connect-form').attr("action", "//lifechurch.us9.list-manage.com/subscribe/post?u=bf7521ed7f8fef2ec1886bb9a&amp;id=82368695a9");
+        $('#mce-LOCATION').attr("value", "Wichita");
+      } else if ($('.location-chooser select').val() == "kansascity") {
+        $('.connect-form').attr("action", "//lifechurch.us9.list-manage.com/subscribe/post?u=bf7521ed7f8fef2ec1886bb9a&amp;id=32164b3882");
+        $('#mce-LOCATION').attr("value", "Kansas City");
+      } else if ($('.location-chooser select').val() == "wpb") {
+        $('.connect-form').attr("action", "//lifechurch.us9.list-manage.com/subscribe/post?u=bf7521ed7f8fef2ec1886bb9a&amp;id=5bdd8528a9");
+        $('#mce-LOCATION').attr("value", "West Palm Beach");
+      }
+    });
+
+    if ($('.location-identifier:contains("wichita")').length > 0) {
+      $('.location-chooser').remove();
+      $('.connect-form').attr("action", "//lifechurch.us9.list-manage.com/subscribe/post?u=bf7521ed7f8fef2ec1886bb9a&amp;id=82368695a9");
     }
 
-    if($('.location-identifier').contains("wichita")) {
-      $('.form-wichita').show();
+    if ($('.location-identifier:contains("kansascity")').length > 0) {
+      $('.location-chooser').remove();
+      $('.connect-form').attr("action", "//lifechurch.us9.list-manage.com/subscribe/post?u=bf7521ed7f8fef2ec1886bb9a&amp;id=32164b3882");
     }
 
-    if($('.location-identifier').contains("kansascity")) {
-      $('.form-kansascity').show();
-    }
-
-    if($('.location-identifier').contains("westpalmbeach")) {
-      $('.form-westpalmbeach').show();
+    if ($('.location-identifier:contains("wpb")').length > 0) {
+      $('.location-chooser').remove();
+      $('.connect-form').attr("action", "//lifechurch.us9.list-manage.com/subscribe/post?u=bf7521ed7f8fef2ec1886bb9a&amp;id=5bdd8528a9");
     }
   }
-
-}
+});
