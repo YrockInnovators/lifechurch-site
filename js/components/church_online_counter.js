a8f039f48641c17a5729dde7361f5975db049603
Zach Gilliam <zach.gilliam@lifechurch.tv>
2015-11-04 16:29:02 -0600

    Update church_online_counter.js
    Change the Church Online API call to life.church

--- js/components/church_online_counter.js
+++ js/components/church_online_counter.js
@@ -20,7 +20,7 @@ jQuery(function() {
   seconds = void 0;
   intervalId = void 0;
   return $.ajax({
-    url: "http://live.lifechurch.tv/api/v1/events/current",
+    url: "http://live.life.church/api/v1/events/current",
     dataType: "json",
     success: function(data) {
       var date, dateString, seconds_till;
