1d8d91d99edd0323638fd94593aa142636fd0d3a
jbatphoto <justin.battenfield@lifechurch.tv>
2016-02-22 10:35:04 -0600

    restructure page for cleanup and adjusted line height for title

---
+++ js/pages/easter.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  $('#simple-menu').sidr();
+  $('#close').sidr();
+  $('#close-2').sidr();
+});
+    $('#fl').click(function() {
+	    $('.fl').toggle('slow');
+	    return false;
+	});
+    $('#ks').click(function() {
+	    $('.ks').toggle('slow');
+	    return false;
+	});
+    $('#nm').click(function() {
+	    $('.nm').toggle('slow');
+	    return false;
+	});
+    $('#ny').click(function() {
+	    $('.ny').toggle('slow');
+	    return false;
+	});
+    $('#ok').click(function() {
+	    $('.ok').toggle('slow');
+	    return false;
+	});
+    $('#tn').click(function() {
+	    $('.tn').toggle('slow');
+	    return false;
+	});
+    $('#tx').click(function() {
+      $(".tx").toggle("slow");
+	    return false;
+	});
+    $('#tul').click(function() {
+	    $('.tul').toggle('slow');
+	    return false;
+	});
+    $('#okc').click(function() {
+	    $('.okc').toggle('slow');
+	    return false;
+	});
