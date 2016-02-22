8a0ba9001a4d6f873441f8d3916d7e054d5a46b2
jbatphoto <justin.battenfield@lifechurch.tv>
2016-02-22 12:20:14 -0600

    add all service times and js tags related to each

--- js/pages/easter.js
+++ js/pages/easter.js
@@ -3,39 +3,155 @@ $(document).ready(function() {
   $('#close').sidr();
   $('#close-2').sidr();
 });
-    $('#fl').click(function() {
+
+// Florida
+$('#fl').click(function() {
 	    $('.fl').toggle('slow');
 	    return false;
 	});
-    $('#ks').click(function() {
+$('.fl').click(function() {
+	    $('.wel').toggle('slow');
+	    return false;
+	});
+
+// Kansas
+$('#ks').click(function() {
 	    $('.ks').toggle('slow');
 	    return false;
 	});
+$('.ks').click(function() {
+	    $('.wch').toggle('slow');
+	    return false;
+	});
+
+// New Mexico
     $('#nm').click(function() {
 	    $('.nm').toggle('slow');
 	    return false;
 	});
+$('.nm').click(function() {
+	    $('.rrn').toggle('slow');
+	    return false;
+	});
+
+// New York
     $('#ny').click(function() {
 	    $('.ny').toggle('slow');
 	    return false;
 	});
+$('.ny').click(function() {
+	    $('.alb').toggle('slow');
+	    return false;
+	});
+
+// Oklahoma
     $('#ok').click(function() {
 	    $('.ok').toggle('slow');
 	    return false;
 	});
+// Shawnee
+$('#shw').click(function() {
+	    $('.shw').toggle('slow');
+	    return false;
+	});
+// Stillwater
+$('#stw').click(function() {
+	    $('.stw').toggle('slow');
+	    return false;
+	});
+
+// Tennessee
     $('#tn').click(function() {
 	    $('.tn').toggle('slow');
 	    return false;
 	});
+$('.tn').click(function() {
+	    $('.hnv').toggle('slow');
+	    return false;
+	});
+
+// Texas
     $('#tx').click(function() {
       $(".tx").toggle("slow");
 	    return false;
 	});
+$('#klr').click(function() {
+	    $('.klr').toggle('slow');
+	    return false;
+	});
+$('#ftw').click(function() {
+	    $('.ftw').toggle('slow');
+	    return false;
+	});
+
+
     $('#tul').click(function() {
 	    $('.tul').toggle('slow');
 	    return false;
 	});
+$('#bao').click(function() {
+	    $('.bao').toggle('slow');
+	    return false;
+	});
+$('#jnk').click(function() {
+	    $('.jnk').toggle('slow');
+	    return false;
+	});
+$('#ows').click(function() {
+	    $('.ows').toggle('slow');
+	    return false;
+	});
+$('#tulsa').click(function() {
+	    $('.tulsa').toggle('slow');
+	    return false;
+	});
+$('#sto').click(function() {
+	    $('.sto').toggle('slow');
+	    return false;
+	});
+$('#sba').click(function() {
+	    $('.sba').toggle('slow');
+	    return false;
+	});
+
     $('#okc').click(function() {
 	    $('.okc').toggle('slow');
 	    return false;
-	});
+  	});
+
+  $('#bnb').click(function() {
+	    $('.bnb').toggle('slow');
+	    return false;
+	}); 
+    $('#edm').click(function() {
+	    $('.edm').toggle('slow');
+	    return false;
+	});   
+    $('#mwc').click(function() {
+	    $('.mwc').toggle('slow');
+	    return false;
+	});   
+    $('#mor').click(function() {
+	    $('.mor').toggle('slow');
+	    return false;
+	});   
+    $('#mus').click(function() {
+	    $('.mus').toggle('slow');
+	    return false;
+	}); 
+    $('#noc').click(function() {
+	    $('.noc').toggle('slow');
+	    return false;
+	}); 
+    $('#okc-2').click(function() {
+	    $('.okc-2').toggle('slow');
+	    return false;
+	}); 
+    $('#soc').click(function() {
+	    $('.soc').toggle('slow');
+	    return false;
+	});  
+    $('#ykn').click(function() {
+	    $('.ykn').toggle('slow');
+	    return false;
+	});   
