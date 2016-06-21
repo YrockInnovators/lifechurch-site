fe0c05ed6b939d53c63ca3edfdcecab0c5d6b1d8
Daniel Ross Woodward <daniel.r.woodward@gmail.com>
2016-06-21 11:42:49 -0500

    Updated CHOP Link to https

--- js/components/church_online_counter.js
+++ js/components/church_online_counter.js
@@ -20,7 +20,7 @@ jQuery(function() {
   seconds = void 0;
   intervalId = void 0;
   return $.ajax({
-    url: "http://live.life.church/api/v1/events/current",
+    url: "https://lifechurch-tv.churchonline.org/api/v1/events/current",
     dataType: "json",
     success: function(data) {
       var date, dateString, seconds_till;
